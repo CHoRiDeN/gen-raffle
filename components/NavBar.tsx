2e0d4a83c5ca8680299a8782b7a8acea72438bcd
danijimenez <renderdani+github@gmail.com>
2025-08-07 15:21:32 +0200

    fixed mobile version

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from "@clerk/nextjs"
-import { WalletIcon } from "lucide-react"
+import { Menu, WalletIcon } from "lucide-react"
 import Link from "next/link"
 import { useUserContext } from "@/contexts/DbUserContext"
 import { Button } from "./ui/button"
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { auth } from "@clerk/nextjs/server"
 
 export default function NavBar() {
     const { dbUser, loading } = useUserContext()
@@ -15,7 +17,7 @@ export default function NavBar() {
             <div className="max-w-7xl mx-auto flex items-center justify-between">
                 <div className="flex items-center space-x-8">
                     <div className="text-purple-600 font-semibold text-xl">GenRaffle*</div>
-                    <nav className="flex items-center space-x-6 text-gray-600">
+                    <nav className="flex items-center space-x-6 text-gray-600 hidden md:flex">
                         <Link href="/">
                             <span className="text-sm">Explore</span>
                         </Link>
@@ -25,7 +27,7 @@ export default function NavBar() {
 
                     </nav>
                 </div>
-                <div className="flex items-center space-x-4">
+                <div className="flex items-center space-x-4 hidden md:flex">
                     <SignedOut>
                         <SignInButton >
                             <Button size="sm" variant="outline">
@@ -50,6 +52,65 @@ export default function NavBar() {
                         <UserButton />
                     </SignedIn>
                 </div>
+
+                <div className="block md:hidden">
+                    <div className="flex items-center justify-between">
+
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button variant="outline" size="icon">
+                                    <Menu className="size-4" />
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent className="overflow-y-auto">
+
+                                <div className="my-6 flex flex-col gap-6 p-4">
+                                    <SignedIn>
+                                        <UserButton />
+                                    </SignedIn>
+                                    <Link href="/">
+                                        <span className="text-base">Explore</span>
+                                    </Link>
+                                    <Link href="/my-raffles">
+                                        <span className="text-base">My raffles</span>
+                                    </Link>
+
+
+
+
+                                </div>
+                                <SheetFooter>
+                                    <SignedOut>
+                                        <SignInButton >
+                                            <Button size="sm" variant="outline">
+                                                Sign In
+                                            </Button>
+                                        </SignInButton>
+                                        <SignUpButton>
+                                            <Button size="sm">
+                                                Register
+                                            </Button>
+                                        </SignUpButton>
+                                    </SignedOut>
+                                    <SignedIn>
+                                        {loading ? (
+                                            <span className="text-sm text-gray-400">Loading...</span>
+                                        ) : dbUser?.wallet_address ? (
+                                            <span className="text-sm text-gray-600 font-mono flex items-center gap-2">
+                                                <WalletIcon className="w-4 h-4" />
+                                                {dbUser.wallet_address.slice(0, 6)}...{dbUser.wallet_address.slice(-4)}
+                                            </span>
+                                        ) : null}
+
+                                    </SignedIn>
+                                </SheetFooter>
+                            </SheetContent>
+
+                        </Sheet>
+                    </div>
+                </div>
             </div>
         </header>)
 }
+
+
